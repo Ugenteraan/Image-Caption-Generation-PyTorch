3064ac758117f8be47dce5dd3063439b92282bc2
ugenteraan <m.ugenteraan15@gmail.com>
2023-05-02 00:29:44 +0800

    save model logic

--- Vanilla-RNN/train.py
+++ Vanilla-RNN/train.py
@@ -13,37 +13,34 @@ from rnn_model import VanillaRNN
 from torchsummary import summary
 from torch.optim import Adam
 from torch.nn.functional import one_hot
-
+import cfg
 
 def main():
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else 'cpu')
 
     train_dataset = VizWiz(mode='train')
     # test_dataset = VizWiz(mode='test')
 
-    train_generator = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=4)
+    train_generator = DataLoader(train_dataset, batch_size=cfg.BATCH_SIZE, shuffle=cfg.SHUFFLE, num_workers=cfg.NUM_WORKERS)
     # test_generator = DataLoader(test_dataset, batch_size=1, shuffle=True, num_workers=4)
 
 
-    #30522 is the size of the vocab set of BERT.
-    vanilla_rnn_model = VanillaRNN(hidden_input_size=49152, hidden_layer_size=2048, input_layer_size=3072, output_layer_size=30522)
-    optimizer = Adam(vanilla_rnn_model.parameters(), lr=0.001)
+    vanilla_rnn_model = VanillaRNN(hidden_input_size=cfg.HIDDEN_FEATURE_SIZE, hidden_layer_size=cfg.HIDDEN_LAYER_SIZE, input_layer_size=cfg.INPUT_LAYER_SIZE, output_layer_size=cfg.OUTPUT_LAYER_SIZE)
 
-    criterion = torch.nn.CrossEntropyLoss()
-    vanilla_rnn_model = vanilla_rnn_model.to(device)
-#    summary(vanilla_rnn_model, (1, 3072, 2048))
+    optimizer = Adam(vanilla_rnn_model.parameters(), lr=cfg.LEARNING_RATE)
 
-    for epoch_idx in range(5):
+    criterion = torch.nn.CrossEntropyLoss(reduction='sum')
+    vanilla_rnn_model = vanilla_rnn_model.to(cfg.DEVICE)
+
+    for epoch_idx in range(10):
 
 
         vanilla_rnn_model.train()
         epoch_loss = 0
         for idx, sample in tqdm(enumerate(train_generator)):
 
-            if idx == 100:
-                break
-            batch_image_feature, batch_caption_embedding, batch_tokens = sample['image_fv'].to(device), sample['caption_embedding'], sample['tokens_target']
+
+            batch_image_feature, batch_caption_embedding, batch_tokens = sample['image_fv'].to(cfg.DEVICE), sample['caption_embedding'], sample['tokens_target']
 
 
             batch_tokens_target = batch_tokens.reshape(-1)
@@ -55,9 +52,7 @@ def main():
             optimizer.zero_grad()
             for seq_idx in range(length):
 
-                batch_caption_embedding_t = batch_caption_embedding[seq_idx].to(device) #current caption embedding vector.
-                batch_token_target_t = batch_tokens_target[seq_idx] #current token integer.
-                # batch_token_target_t_one_hot = one_hot(batch_token_target_t, num_classes=30522).unsqueeze(0).float().to(device)
+                batch_caption_embedding_t = batch_caption_embedding[seq_idx].to(cfg.DEVICE) #current caption embedding vector.
 
                 output_t, hidden_state_t = vanilla_rnn_model(batch_caption_embedding_t, hidden_state_t)
 
@@ -67,22 +62,23 @@ def main():
                     output_sequences = torch.concat([output_sequences, output_t.reshape(1, -1)], dim=0)
 
 
-            batch_token_target_t_one_hot = one_hot(batch_tokens_target, num_classes=30522).float().to(device)
-            # print(output_sequences, batch_token_target_t_one_hot)
-            # print(output_sequences.size(), batch_token_target_t_one_hot.size())
+            batch_token_target_t_one_hot = one_hot(batch_tokens_target, num_classes=cfg.NUM_TOKENS).float().to(cfg.DEVICE)
+
             sample_loss = criterion(input=output_sequences, target=batch_token_target_t_one_hot)
 
-            # print(sample_loss, sample_loss.size())
             sample_loss.backward()
+
             optimizer.step()
             epoch_loss += sample_loss.item()
+
         print(f"Loss on epoch {epoch_idx} is {epoch_loss}")
+        torch.save(vanilla_rnn_model, './vanilla-rnn-model.pth')
 
 
     vanilla_rnn_model.eval()
 
-    dummy_input_caption_embedding = torch.randn(1, 3072)
-    dummy_input_image_feature = torch.randn(1, 49152)
+    dummy_input_caption_embedding = torch.randn(1, 3072).to(cfg.DEVICE)
+    dummy_input_image_feature = torch.randn(1, 49152).to(cfg.DEVICE)
 
     dummy_output,hidden_state = vanilla_rnn_model(dummy_input_caption_embedding, dummy_input_image_feature)
 
